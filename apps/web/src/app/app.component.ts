9a671f1fef85d917d60a7382e5ea183693244d3f
s-duu-jp <s@duu.jp>
2022-05-28 19:49:26 +0900

    コンポーネントクラスによるリアクティブな状態管理

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,8 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter } from "@angular/core";
+import { BehaviorSubject, takeUntil } from "rxjs";
 
 @Component({
   selector: "product-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent {
+  onDestroy$ = new EventEmitter();
+  state$ = new BehaviorSubject<{ hoge: boolean }>({ hoge: true });
+
+  ngOnInit(): void {
+    this.state$.pipe(takeUntil(this.onDestroy$)).subscribe((state) => {
+      state.hoge = !state.hoge;
+      console.log(`値が変わったよ！：${state.hoge}`);
+    });
+  }
+
+  setState(): void {
+    this.state$.next({ hoge: this.state$.getValue().hoge });
+  }
+
+  getState() {
+    console.log(this.state$.getValue());
+  }
+
+  ngOnDestroy() {
+    console.log(`購読やめたよ！`);
+    this.onDestroy$.emit();
+  }
+}
