5dcf87242174d88bd1558cb847c456c22e7c60b3
s-duu-jp <s@duu.jp>
2022-05-28 18:46:20 +0900

    コンポーネントクラスによる直接の状態管理

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent {
+  message = String(new Date());
+
+  updateMessage() {
+    this.message = String(new Date());
+  }
+}
